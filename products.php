a645e40d1e47a8de0619846c9efa55fa5e41df95
anggrainirekaa <anggraini137reka@gmail.com>
2026-09-20 02:32:09 +0700

    Add files via upload

---
+++ products.php
@@ -0,0 +1,54 @@
+<?php
+// DATA LAYER
+// Menyimpan data produk dalam array asosiatif multidimensi (tanpa database).
+
+$products = [
+    [
+        "id"        => 1,
+        "nama"      => "Laptop 14 inci",
+        "kategori"  => "Elektronik",
+        "harga"     => 7500000,
+        "stok"      => 5,
+        "deskripsi" => "Laptop ringan untuk kuliah dan pekerjaan kantor."
+    ],
+    [
+        "id"        => 2,
+        "nama"      => "Mouse Wireless",
+        "kategori"  => "Aksesoris Komputer",
+        "harga"     => 150000,
+        "stok"      => 2,
+        "deskripsi" => "Mouse nirkabel dengan penerima USB."
+    ],
+    [
+        "id"        => 3,
+        "nama"      => "Keyboard Mechanical",
+        "kategori"  => "Aksesoris Komputer",
+        "harga"     => 450000,
+        "stok"      => 8,
+        "deskripsi" => "Keyboard mekanik dengan lampu latar."
+    ],
+    [
+        "id"        => 4,
+        "nama"      => "Flashdisk 64 GB",
+        "kategori"  => "Penyimpanan",
+        "harga"     => 85000,
+        "stok"      => 1,
+        "deskripsi" => "Flashdisk USB dengan kapasitas 64 GB."
+    ],
+    [
+        "id"        => 5,
+        "nama"      => "Monitor LED 24 inci",
+        "kategori"  => "Elektronik",
+        "harga"     => 1800000,
+        "stok"      => 3,
+        "deskripsi" => "Monitor Full HD untuk penggunaan sehari-hari."
+    ],
+    [
+        "id"        => 6,
+        "nama"      => "Headset Gaming",
+        "kategori"  => "Audio",
+        "harga"     => 275000,
+        "stok"      => 0,
+        "deskripsi" => "Headset dengan mikrofon untuk bermain game dan meeting online."
+    ]
+];
